8174c65d643248f823dca3952de74bc14688c1bf
Ritheesh <ritheesh.4@gmail.com>
2020-05-07 16:25:56 +0530

    initial commits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+
+class Counter extends React.Component {
+  state = {
+    count: 0,
+    classNameButton : "button"
+  };
+  handleClick = () => {
+    this.setState(({ count }) => ({
+      count: count + 1
+    }));if( this.state.count > 1){
+      this.setState({classNameButton}) ({
+        classNameButton: "button-one"
+      })
+    }
+    
+  };
+  
+  render() {
+    return <button onClick={this.handleClick} className = {this.state.classNameButton}>{this.state.count}</button>;
+  }
+}
+
+
+class Decrementer extends React.Component {
+  state = {
+    count: 10
+  };
+  handleClick = () => {
+    this.setState(({ count }) => ({
+      count: count -1
+    }));
+  };
+  render() {
+    return <button onClick={this.handleClick}>{this.state.count}</button>;
+  }
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <div className="App">
+        <h1>Hello</h1>
+        <div className="flex-container">
+          <div>hi</div>
+          <div>hi</div>
+          <div>hi</div>
+        </div>
+        <div className="flex-container">
+          <div>hello</div>
+          <div>hello</div>
+        </div>
+        <div className="flex-container-two">
+          <div className="box">
+            <div>this is one</div>
+            <div>this is two</div>
+            <div>this is two</div>
+          </div>
+          <div className="box">
+            <div>this is one</div>
+            <div>this is two</div>
+            <div>this is two</div>
+          </div>
+         
+        </div>
+     <Counter className ={this.state.classNameButton}/>
+     <Decrementer/>
+
+      </div>
+    </header>
   );
 }
 
